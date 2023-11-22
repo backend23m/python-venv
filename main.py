b82c6ccd64a0f0378a98abd856a0508e474612f8
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-22 12:18:23 +0500

    users

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import requests
 import json
+from pprint import pprint
 
-response = requests.get('https://randomuser.me/api/')
 
-if response.status_code == 200:
-    content = response.content
-    data = json.loads(content.decode())
+def get_randomuser() -> dict:
+    response = requests.get('https://randomuser.me/api/')
+    
+    if response.status_code == 200:
+        content = response.content
+        data = json.loads(content.decode())
+    
+        randomuser = data['results'][0]
+    
+        user = {
+            "fullname": randomuser['name']['first'] + " " + randomuser['name']['last'],
+            "email": randomuser['email'],
+            "phone": randomuser['phone']
+        }
+    
+    return user
 
-    randomuser = data['results'][0]
+def get_randomusers(n: int) -> list:
+    users = []
+    for i in range(n):
+        user = get_randomuser()
+        users.append(user)
 
-    user = {
-        "fullname": randomuser['name']['first'] + " " + randomuser['name']['last'],
-        "email": randomuser['email']
+    result = {
+        "users": users,
+        "info": {"count": n}
     }
-    print(user)
+    return result
+
+def write_data(result: dict):
+    with open('users.json', 'w') as f:
+        data = json.dumps(result, indent=4)
+        f.write(data)
+
+result = get_randomusers(30)
+write_data(result)
