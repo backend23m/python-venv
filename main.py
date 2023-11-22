f670d09cba008f2d3786ede689c665385303b984
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-22 11:19:26 +0500

    randomuser

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ if response.status_code == 200:
     content = response.content
     data = json.loads(content.decode())
 
+    randomuser = data['results'][0]
+
     user = {
-        "fullname": data['results'][0]['name']['first'] + " " + data['results'][0]['name']['last']
+        "fullname": randomuser['name']['first'] + " " + randomuser['name']['last'],
+        "email": randomuser['email']
     }
     print(user)
