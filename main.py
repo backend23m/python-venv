fe40258a6bf26060a58a44ad4b9b63806cd00c9e
jasminatarmasheva786 <jasminatarmasheva786@gmail.com>
2023-11-24 09:35:22 +0500

    Finished

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import requests
 import json
+from pprint import pprint
+users={}
+n=int(input())
+gender = input()
+lst=[]
+info=0
 
-response = requests.get('https://randomuser.me/api/')
+while info != n:
+    response = requests.get('https://randomuser.me/api/')
+    user={}
 
-if response.status_code == 200:
-    content = response.content
-    data = json.loads(content.decode())
-
-    print(data)
-    print(type(data))
+    if response.status_code == 200:
+        content = response.content
+        data = json.loads(content.decode())['results'][0]
+        if data['gender']==gender:
+            user["fullname"]=f"Name: {data['name']['title']} {data['name']['first']} {data['name']['last']}"
+            user["number"]=f"Number: {data['phone']}"
+            user['email']=f"email: {data['email']}"
+            lst.append(user)
+            info+=1
+f=open('users.json','w')
+users['users']=lst
+users['info']=info
+f.write(json.dumps(users,indent=4))
+f.close()
